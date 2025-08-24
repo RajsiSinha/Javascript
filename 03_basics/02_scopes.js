3a65a16411e29317736b2524f6be63dbc676d85a
Rajsi Sinha <132333864+RajsiSinha@users.noreply.github.com>
2025-08-24 10:05:27 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,28 @@
+//let a = 10
+const b = 30
+var c = 40
+
+if(true){
+    //let a = 20
+    const b = 60
+    var c = 80
+    
+}
+
+//console.log(a); // 10
+console.log(b); // 30
+console.log(c); // 80
+
+let a = 100
+if(true){
+    let a = 10
+    const b = 20
+    console.log("innr:", a); // 10
+}
+console.log(a); // 100
+
+
+
+
+
+
