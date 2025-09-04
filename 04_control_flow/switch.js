4c1a12d11df76cf7ead4b2c9708d969e3ed4b979
Rajsi Sinha <132333864+RajsiSinha@users.noreply.github.com>
2025-09-04 12:13:16 +0000

    condition and switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key){
+//     case value:
+
+//         break;
+    
+//     default:
+
+//         break;
+// }
+
+const month = 4
+switch (month){
+    case 1:
+        console.log("january");
+         break;
+    
+    case 2:
+        console.log("april");
+         break;
+
+    case 2:
+        console.log("june");
+         break;
+
+    default:
+        console.log("none matched");
+        break;
+ }
+
+const month2 = "apr"
+switch (month2){
+    case "jan":
+        console.log("january");
+         break;
+    
+    case "apr":
+        console.log("april");
+         break;
+
+    case "jun":
+        console.log("june");
+         break;
+
+    default:
+        console.log("none matched");
+        break;
+ }
+
+
+
+
+
+
