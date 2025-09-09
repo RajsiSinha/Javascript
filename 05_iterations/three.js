7aeb5fbb121be1d48ff22dd67a359bbd3abdc489
Rajsi Sinha <132333864+RajsiSinha@users.noreply.github.com>
2025-09-09 10:36:03 +0000

    loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,42 @@
+// for of
+
+["", "", ""]
+[{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "hello world!"
+for (const greet of greetings) {
+    console.log(`each char is ${greet}`);
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India") // duplicate is not added
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+}
+
+const myObj = {
+    'game1' : 'NFS',
+    'game2' : 'spiderman'
+}
+
+// for (const [key, value] of myObj) {
+//     console.log(key, ':-', value); // error in case of object
+// }
+
+
+
+
