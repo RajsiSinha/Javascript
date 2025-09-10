eb2565b7a25a5bc74b8af3bfa7f00be3adc18a02
Rajsi Sinha <132333864+RajsiSinha@users.noreply.github.com>
2025-09-10 10:11:52 +0000

    Reduce

---
+++ 05_iterations/eight.js
@@ -0,0 +1,29 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0) // 0 is value of accumulator
+
+const myTotal = myNums.reduce( (acc, currval) => acc + currval, 0)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 1999
+    },
+    {
+        itemName: "ui/ux course",
+        price: 999
+    },
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
